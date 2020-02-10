fe001d2cf1838deb02f657270033c92f46771de1
Thiago Tavares <twtavares@gmail.com>
2020-02-10 16:53:28 -0300

    feat(lib): criacao de lib-common e componente footer

---
+++ projects/lib-common/src/lib/lib-common.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    FooterComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    FooterComponent
+  ]
+})
+export class LibCommonModule { }
---
+++ projects/lib-common/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of lib-common
+ */
+
+export * from './lib/lib-common.module';
+export * from './lib/footer/footer.component';
