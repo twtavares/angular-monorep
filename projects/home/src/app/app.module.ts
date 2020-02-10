9bc14221b7c7c8e642244370a5f38d1ac192d55f
Thiago Tavares <twtavares@gmail.com>
2020-02-10 18:03:29 -0300

    feat(angular material): instala angular material nas apps home e profile

--- projects/home/src/app/app.module.ts
+++ projects/home/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { LibCommonModule } from '@lib/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LibCommonModule
+    LibCommonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/lib-material/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of lib-material
+ */
+
+export * from './lib/lib-material.module';
